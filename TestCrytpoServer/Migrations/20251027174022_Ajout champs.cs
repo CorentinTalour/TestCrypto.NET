0b5824f43ffe8e3e310707f69f9d0326099b8e5d
Corentin Talour <corentintalour@gmail.com>
2025-10-28 16:00:05 +0100

    Add migration for Vault table and update Secrets schema with new fields

---
+++ TestCrytpoServer/Migrations/20251027174022_Ajout champs.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestCrytpoServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Ajoutchamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CipherNameB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CipherNotesB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CipherURLB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CipherNameB64",
+                table: "Secrets");
+
+            migrationBuilder.DropColumn(
+                name: "CipherNotesB64",
+                table: "Secrets");
+
+            migrationBuilder.DropColumn(
+                name: "CipherURLB64",
+                table: "Secrets");
+        }
+    }
+}
---
+++ TestCrytpoServer/Migrations/20251027180204_Test.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestCrytpoServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Test : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CipherURLB64",
+                table: "Secrets",
+                newName: "CipherUrlB64");
+
+            migrationBuilder.RenameColumn(
+                name: "IvB64",
+                table: "Secrets",
+                newName: "IvUrlB64");
+
+            migrationBuilder.RenameColumn(
+                name: "CiphertextB64",
+                table: "Secrets",
+                newName: "IvPasswordB64");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CipherPasswordB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IvNameB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IvNotesB64",
+                table: "Secrets",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CipherPasswordB64",
+                table: "Secrets");
+
+            migrationBuilder.DropColumn(
+                name: "IvNameB64",
+                table: "Secrets");
+
+            migrationBuilder.DropColumn(
+                name: "IvNotesB64",
+                table: "Secrets");
+
+            migrationBuilder.RenameColumn(
+                name: "CipherUrlB64",
+                table: "Secrets",
+                newName: "CipherURLB64");
+
+            migrationBuilder.RenameColumn(
+                name: "IvUrlB64",
+                table: "Secrets",
+                newName: "IvB64");
+
+            migrationBuilder.RenameColumn(
+                name: "IvPasswordB64",
+                table: "Secrets",
+                newName: "CiphertextB64");
+        }
+    }
+}
